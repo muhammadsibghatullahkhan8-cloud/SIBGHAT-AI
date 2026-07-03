5f797773b801018d96be9bd627de99b5c82f5941
muhammadsibghatullahkhan8-cloud <muhammadsibghatullahkhan8@gmail.com>
2026-07-03 22:12:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 
+const prompt = document.getElementById("prompt");
+const generateBtn = document.getElementById("generateBtn");
+const resultImage = document.getElementById("resultImage");
+const downloadBtn = document.getElementById("downloadBtn");
+const loading = document.getElementById("loading");
+
+generateBtn.addEventListener("click", async () => {
+
+    const text = prompt.value.trim();
+
+    if(text === ""){
+        alert("Please enter a prompt.");
+        return;
+    }
+
+    loading.classList.remove("hidden");
+
+    resultImage.style.display="none";
+    downloadBtn.classList.add("hidden");
+
+    const imageURL =
+    "https://image.pollinations.ai/prompt/" +
+    encodeURIComponent(text);
+
+    resultImage.src=imageURL;
+
+    resultImage.onload=()=>{
+
+        loading.classList.add("hidden");
+
+        resultImage.style.display="block";
+
+        downloadBtn.href=imageURL;
+
+        downloadBtn.classList.remove("hidden");
+
+    };
+
+});
